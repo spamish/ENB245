7896659ee917fcbe506eaf190b5ca29c9a0b1b6c
Samuel Janetzki <spamwonderfulspam@hotmail.com>
2014-10-10 08:07:07 +1000

    Commented and ready to code.

--- ENB245DVR.asm
+++ ENB245DVR.asm
@@ -15,6 +15,10 @@
 ; ** Author:  Mark Broadmeadow
 ; ** 
 ; ************************************************************
+; Group 412.
+; Alex Fernicola (n8609373)
+; Rebecca Hopping (n8314217)
+; Samuel Janetzki (n7402643)
 
 .include "m32u4def.inc"
 
@@ -64,21 +68,46 @@ Entry:
 
 ; RECORD - Initiates a record sequence (UNFINISHED)
 Record:
+	
+	; Debounce
+
+	; Enable interrupts on INT3 (RED pin).
+
 	call 	Buffer_ResetReadPointer
 	call 	Buffer_ResetWritePointer
 	call 	TWI_ResetPagePointers
 	call 	ADC_Start
+
+	; Insert code for RECORD functionality here.
+
+	; Disable interrupts on INT3.
+
 	ret
 
 ; PLAY - Initiates a playback sequence (UNFINISHED)
 Play:
-	; Insert code for PLAY functionality here
+	
+	; Debounce
+
+	; Enable interrupts on INT2 (GREEN pin).
+
+	; Insert code for PLAY functionality here.
+
+	; Disable interrupts on INT2.
+
 	ret
 
 ; STOP - Stops all playback or recording (UNFINISHED)
 Stop:
+	
+	; Insert code for STOP functionality here.
+
+	; Stop PWM
+
 	call 	ADC_Stop
-	ret
+
+	;rjmp Main
+	ret ;Replace with rjump;
 	
 
 ; **
@@ -129,7 +158,12 @@ Init:
 	call Buffer_Init
 	call ADC_Init
 	call TWI_Init
-	; Add additional init routines here
+	call PWM_Init
+	call IO_Init
+	call Int_Init
+
+	; Add additional init routines here.
+
 	sei								; Enable interrupts
 	ret
 
@@ -142,16 +176,92 @@ Clock_Init:
 	ret
 
 ; ****************** MAIN CODE BEGINS HERE ******************
+
+PWM_Init:
+	; Initialise PWM pins.
+	
+	; Set clear-on-compare for channel A and
+	; Normal Mode with no prescaling, with a
+	; Forced Output Compare Match set on Register A.
+	ldi r16,(1<<COM1A1)
+	ldi r17,(1<<CS11)
+	ldi r18,(1<<FOC1A)
+	
+	; Push to pins.
+	sts TCCR1A,r16
+	sts TCCR1B,r17
+	sts TCCR1C,r18
+
+	; Set OC1A pin as output.
+	sbi DDRB,DDB5
+
+	; Set desired PWM.
+	
+	; Set clear-on-compare for channel A and
+	; 8 bit Fast PWM mode with fclk/8 prescaling.
+	; PWM clock at 7.84kHz
+	ldi r16,(1<<WGM10)|(1<<COM1A1)
+	ldi r17,(1<<WGM12)|(1<<CS11)
+	ldi r18,0x00
+	
+	; Push to pins.
+	sts TCCR1A,r16
+	sts TCCR1B,r17
+	sts TCCR1C,r18
+
+	; Set default pulse width.
+	ldi r16,0x7F
+	sts OCR1AL,r16
+
+	ret
+
+IO_Init:
+	; Set button pins.
+	
+	; Set pullup resistors and outputs.
+	sbi PORTB,PB4
+	sbi PORTB,PB6
+	; Set direction of I/O.
+	sbi DDRB,DDB4
+	
+	; Set pullup resistors.
+	ldi r16,(1<<PD2)|(1<<PD3)
+
+	; Push to pins.
+	out PORTD,r16
+
+	; Set high outputs.
+	ldi r16,(1<<PF1)|(1<<PF4)|(1<<PF6)
+	; Set direction of I/O.
+	ldi r17,(1<<DDF1)|(1<<DDF4)|(1<<DDF6)
+
+	; Push to pins.
+	out PORTF,r16
+	out DDRF,r17
+	
+	ret
+
+Int_Init:
+	
+	; Set up disabled interrupts on INT2 and INT3.
+
+
+	ret
+
 CodeStart:
 
 	; Initialisation
 	call Init
 
+	nop
+
 Main:
-	
 	; Main loop code goes here
 
+	;sbis PIND,PD3 ; RECORD button press.
+	;rjmp Record
+	
+	;sbis PIND,PD2 ; PLAY button press.
+	;rjmp Play
+
 	rjmp Main
-
-
-
